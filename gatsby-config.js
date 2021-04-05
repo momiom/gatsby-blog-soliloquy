7616100fa3687aee070bd56ed8f6c4b6f91d01f9
momiom <kazuki.k.bl8+github.momio@gmail.com>
2021-04-05 23:50:22 +0900

    gatsby の plugin 類を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,63 @@
+const path = require('path');
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
-  plugins: [{ resolve: `gatsby-plugin-emotion` }],
+  siteMetadata: {
+    title: `Soliloquy ひとりごと`,
+    author: {
+      name: `Reika Nakayama`,
+      summary: `Soliloquy ひとりごと`,
+    },
+    description: `Soliloquy ひとりごと`,
+    siteUrl: `https://soliloquy.com`,
+    social: {
+      twitter: `soliloquy`,
+    },
+  },
+  plugins: [
+    // `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Soliloquy ひとりごと`,
+        short_name: `Soliloquy`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.API_KEY,
+        serviceId: process.env.SERVICE_ID,
+        apis: [
+          {
+            endpoint: "posts",
+          },
+        ],
+      },
+    },
+  ],
 }
