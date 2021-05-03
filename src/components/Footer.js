847b57f05af57c39169420f2c5cc9ece574c0fe9
momiom <kazuki.k.bl8+github.momio@gmail.com>
2021-05-03 21:16:55 +0900

    マークアップ: フッター

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,27 @@
 import React from 'react'
-import  tw from 'twin.macro'
+import { useStaticQuery, graphql } from 'gatsby'
+import {css} from 'twin.macro'
+import dayjs from 'dayjs'
 
-const Footer = () => (
-  <div>Footer</div>
-)
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMicrocmsProfile {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+  `)
+  const year = dayjs().format('YYYY')
+  const name = data.allMicrocmsProfile.edges[0].node.name
+  return (
+    <div tw="text-sm text-light-black flex justify-center items-center">
+      <small>©︎ {year} {name}</small>
+    </div>
+  )
+}
 
 export default Footer
