3f7fff9fb9efd8db1ce454f19348b4a9c2eb59e5
momiom <kazuki.k.bl8+github.momio@gmail.com>
2021-06-05 00:22:36 +0900

    ページングの前後矢印のスタイリング

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
-import 'twin.macro'
+import tw, { css } from 'twin.macro'
 import { Layout, PostList } from '../components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faChevronRight,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons'
 
 const IndexPage = ({ data, pageContext }) => {
   const { currentPage, numPages } = pageContext
@@ -15,18 +20,45 @@ const IndexPage = ({ data, pageContext }) => {
     <Layout seo={{ title: 'Home' }}>
       <PostList edges={data.allMicrocmsPosts.edges} />
 
-      <div tw="flex justify-between">
-        {!isFirst && (
-          <Link to={prevPage} rel="prev">
-            ← Previous Page
-          </Link>
-        )}
+      <div tw="flex justify-between pt-4">
+        <Link
+          to={prevPage}
+          rel="prev"
+          tw="bg-white shadow rounded-lg overflow-hidden w-full z-10 text-center px-2.5 py-2 hover:bg-gray-100 transition"
+          css={css`
+            width: 10ch;
+            color: #505050;
+            ${isFirst ? tw`invisible` : ``}
+          `}
+        >
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            tw=""
+            css={css`
+              font-size: 0.9em;
+            `}
+          />
+          <span tw="pl-1.5">Prev</span>
+        </Link>
 
-        {!isLast && (
-          <Link to={nextPage} rel="next">
-            Next Page →
-          </Link>
-        )}
+        <Link
+          to={nextPage}
+          rel="next"
+          tw="bg-white shadow rounded-lg overflow-hidden w-full z-10 text-center px-2.5 py-2 hover:bg-gray-100 transition"
+          css={css`
+            width: 10ch;
+            color: #505050;
+          `}
+        >
+          <span tw="pr-1.5">Next</span>
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            css={css`
+              font-size: 0.9em;
+              ${isLast ? tw`invisible` : ``}
+            `}
+          />
+        </Link>
       </div>
     </Layout>
   )
