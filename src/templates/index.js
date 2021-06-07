d10b6aa5869d02ecaa117220fd788853d6b8828c
momiom <kazuki.k.bl8+github.momio@gmail.com>
2021-06-08 00:11:57 +0900

    記事一覧のページングを修正
    visibility: hidden; ではリンクの先読みが動いてしまい存在しないページをフェッチしようとするため修正

--- src/templates/index.js
+++ src/templates/index.js
@@ -16,49 +16,65 @@ const IndexPage = ({ data, pageContext }) => {
     currentPage - 1 === 1 ? '/' : `/page/${(currentPage - 1).toString()}`
   const nextPage = `/page/${(currentPage + 1).toString()}`
 
+  const prevLink = isFirst ? (
+    <div
+      css={css`
+        width: 10ch;
+        visibility: hidden;
+      `}
+    ></div>
+  ) : (
+    <Link
+      to={prevPage}
+      rel="prev"
+      tw="bg-white shadow rounded-lg overflow-hidden w-full z-10 text-center text-gray-600 px-2.5 py-2 hover:bg-gray-100 transition"
+      css={css`
+        width: 10ch;
+      `}
+    >
+      <FontAwesomeIcon
+        icon={faChevronLeft}
+        tw=""
+        css={css`
+          font-size: 0.9em;
+        `}
+      />
+      <span tw="pl-1.5">Prev</span>
+    </Link>
+  )
+
+  const nextLink = isLast ? (
+    <div
+      css={css`
+        width: 10ch;
+        visibility: hidden;
+      `}
+    ></div>
+  ) : (
+    <Link
+      to={nextPage}
+      rel="next"
+      tw="bg-white shadow rounded-lg overflow-hidden w-full z-10 text-center text-gray-600 px-2.5 py-2 hover:bg-gray-100 transition"
+      css={css`
+        width: 10ch;
+      `}
+    >
+      <span tw="pr-1.5">Next</span>
+      <FontAwesomeIcon
+        icon={faChevronRight}
+        css={css`
+          font-size: 0.9em;
+        `}
+      />
+    </Link>
+  )
   return (
     <Layout seo={{ title: 'Home' }}>
       <PostList edges={data.allMicrocmsPosts.edges} />
 
       <div tw="flex justify-between pt-4">
-        <Link
-          to={prevPage}
-          rel="prev"
-          tw="bg-white shadow rounded-lg overflow-hidden w-full z-10 text-center px-2.5 py-2 hover:bg-gray-100 transition"
-          css={css`
-            width: 10ch;
-            color: #505050;
-            ${isFirst ? tw`invisible` : ``}
-          `}
-        >
-          <FontAwesomeIcon
-            icon={faChevronLeft}
-            tw=""
-            css={css`
-              font-size: 0.9em;
-            `}
-          />
-          <span tw="pl-1.5">Prev</span>
-        </Link>
-
-        <Link
-          to={nextPage}
-          rel="next"
-          tw="bg-white shadow rounded-lg overflow-hidden w-full z-10 text-center px-2.5 py-2 hover:bg-gray-100 transition"
-          css={css`
-            width: 10ch;
-            color: #505050;
-          `}
-        >
-          <span tw="pr-1.5">Next</span>
-          <FontAwesomeIcon
-            icon={faChevronRight}
-            css={css`
-              font-size: 0.9em;
-              ${isLast ? tw`invisible` : ``}
-            `}
-          />
-        </Link>
+        { prevLink }
+        { nextLink }
       </div>
     </Layout>
   )
