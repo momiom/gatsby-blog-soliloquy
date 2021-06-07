658a28fc2e6ec8a5d9da48318da952c6f090b6b5
momiom <kazuki.k.bl8+github.momio@gmail.com>
2021-06-07 23:40:46 +0900

    ページ読み込み時にアイコンフォントが大きく表示されてしまう問題に対応

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,8 +2,6 @@ import { Global, css } from '@emotion/react'
 import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
 import { config } from '@fortawesome/fontawesome-svg-core'
 
-import "@fortawesome/fontawesome-svg-core/styles.css"
-
 config.autoAddCss = false
 
 const customStyles = css`
